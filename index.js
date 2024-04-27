a47b5bf7b5477dbfb61c88244d18ce8b7e1a20fa
Lucas Rios <lucasriosferreira.564@gmail.com>
2024-04-27 12:38:38 -0300

    realização do CRUD das varinhas e função de status de sangue

--- index.js
+++ index.js
@@ -11,17 +11,18 @@ const pool = new Pool({
     host: 'localhost',
     database: 'lucas',
     password: 'ds564',
-    port: 7007,
+    port: 5432, //7007
 });
 
 app.use(express.json());
 
 let sangue = ['puro', 'mestiço', 'trouxa'];
 
-function statusSangue (sangue) {
-    if(!sangue.includes(sangue)) {
-        
+function statusSangue(status) {
+    if (!sangue.includes(status)) {
+        return 'Status de sangue inválido. Os valores permitidos são puro, mestiço ou trouxa.';
     }
+    return '';
 }
 
 app.get('/bruxos', async (req, res) => {
@@ -40,6 +41,10 @@ app.get('/bruxos', async (req, res) => {
 app.post('/bruxos', async(req, res) => {
     try {
         const {nome, idade, casa, habilidade, status, patrono}  = req.body;
+        const erroStatus = statusSangue(status);
+        if (erroStatus) {
+            return res.status(400).send({ mensagem: erroStatus });
+        }
 
         await pool.query('INSERT INTO bruxos (nome, idade, casa, habilidade, status, patrono) VALUES ($1, $2, $3, $4, $5, $6)', [nome, idade, casa, habilidade, status, patrono]);
         res.status(201).send({mensagem: 'Sucesso ao criar bruxo'});
@@ -73,14 +78,95 @@ app.put('/bruxos/:id', async(req, res) => {
     }
 })
 
-app.get('/bruxos/:id', async(req, res) => {
+/*app.get('/bruxos/:id', async(req, res) => {
     try {
         const { id } = req.params;
-        await pool.query('SELECT * FROM usuarios WHERE id = $1', [id]);
-        res.status(200).send({mensagem: `Bruxo com o nome ${nome} encontrado com sucesso`});
+        await pool.query('SELECT * FROM bruxos WHERE id = $1', [id]);
+        res.status(200).send({mensagem: `Bruxo encontrado com sucesso`});
     } catch (error) {
         console.error('Erro ao obter bruxo por id', error);
         res.status(500).send({mensagem: 'Erro ao obter bruxo por id'})
+    }
+});*/
+
+app.get('/bruxos/:nome', async (req, res) => {
+    try {
+        const { nome } = req.params;
+        const resultado = await pool.query('SELECT * FROM bruxos WHERE nome = $1', [nome]);
+        if (resultado.rowCount === 0) {
+            res.status(404).send({mensagem: 'Bruxo não encontrado'});
+        } else {
+            res.status(200).json({
+                total: resultado.rowCount,
+                bruxos: resultado.rows
+            });
+        }
+    } catch (error) {
+        console.error('Erro ao obter bruxo por nome', error);
+        res.status(500).send({mensagem: 'Erro ao obter bruxo por nome'})
+    }
+});
+
+
+
+
+app.get('/varinhas', async (req, res) => {
+    try {
+        const resultado = await pool.query('SELECT * FROM varinhas');
+        res.json({
+            total: resultado.rowCount,
+            varinhas: resultado.rows
+        });
+    } catch (error) {
+        console.error('Erro ao obter todas as varinhas', error);
+        res.status(500).send({mensagem: 'Erro ao obter todas as varinhas'})
+    }
+});
+
+app.post('/varinhas', async(req, res) => {
+    try {
+        const {material, comprimento, nucleo, fabricacao}  = req.body;
+
+        await pool.query('INSERT INTO varinhas (material, comprimento, nucleo, fabricacao) VALUES ($1, $2, $3, $4)', [material, comprimento, nucleo, fabricacao]);
+        res.status(201).send({mensagem: 'Sucesso ao criar varinha'});
+    } catch (error) {
+        console.error('Erro ao criar varinha', error);
+        res.status(500).send({mensagem: 'Erro ao criar varinha'})
+    }
+});
+
+app.delete('/varinhas/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+        await pool.query('DELETE FROM varinhas WHERE id = $1', [id]);
+        res.status(200).send({mensagem: 'Varinha deletada'});
+    } catch (error) {
+        console.error('Erro ao deletar varinha', error);
+        res.status(500).send({mensagem: 'Erro ao deletar varinha'})
+    }
+});
+
+app.put('/varinhas/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+        const {material, comprimento, nucleo, fabricacao}  = req.body;
+
+        await pool.query('UPDATE varinhas SET material = $1, comprimento = $2, nucelo = $3, fabricacao = $4 WHERE id = $5', [material, comprimento, nucleo, fabricacao, id]);
+        res.status(200).send({mensagem: 'Varinha editada com sucesso'})
+    } catch (error) {
+        console.error('Erro ao editar a varinha', error);
+        res.status(500).send({mensagem: 'Erro ao editar a varinha'})
+    }
+})
+
+app.get('/varinhas/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+        await pool.query('SELECT * FROM varinhas WHERE id = $1', [id]);
+        res.status(200).send({mensagem: `Varinha com o ID ${id} encontrada com sucesso`});
+    } catch (error) {
+        console.error('Erro ao obter varinha por id', error);
+        res.status(500).send({mensagem: 'Erro ao obter varinha por id'})
     }
 });
 
